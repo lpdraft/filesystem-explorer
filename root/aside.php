130de8db582c60f25e1b2653581c3cc420e4c46e
Lokesh Pereiro <lpdraft001@gmail.com>
2022-09-15 10:22:30 +0200

    finales css changes

--- root/aside.php
+++ root/aside.php
@@ -1,17 +1,17 @@
 <div class="aside">
 
-<div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+<div class="flex-shrink-0 p-3 bg-light" style="width: 280px;">
     <a href="/" class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
       <svg class="bi pe-none me-2" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
       <span class="fs-5 fw-semibold">Folders</span>
     </a>
-    <ul class="list-unstyled ps-0">
+    <ul class="list-unstyled ps-0" id="ul-style">
       <li class="mb-1">
         <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
           Home
         </button>
         <div class="collapse show" id="home-collapse">
-          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small"   >
            <?php 
             FolderList('../root');
             function FolderList($path){
@@ -45,8 +45,6 @@
             }
             ?>
            
-
-
           </ul>
         </div>
       </li>
